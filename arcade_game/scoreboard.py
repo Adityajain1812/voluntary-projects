d1274591b777c56b3781d125319f736a5c5701a4
adityajain1812 <142815869+Adityajain1812@users.noreply.github.com>
2025-04-12 21:36:20 +0530

    Update scoreboard.py

--- arcade_game/scoreboard.py
+++ arcade_game/scoreboard.py
@@ -12,6 +12,7 @@ class Scoreboard(Turtle):
         self.r_score = 0
         self.update_scoreboard()
 
+    #  Function to update the score as left or right paddle misses
     def update_scoreboard(self):
         self.clear()
         self.goto(-100, 200)
@@ -19,10 +20,12 @@ class Scoreboard(Turtle):
         self.goto(100, 200)
         self.write(self.r_score, align="center", font=("Courier", 80, "normal"))
 
+    # Function to detect miss from right paddle
     def l_point(self):
         self.l_score += 1
         self.update_scoreboard()
 
+    # Function to detect miss from left paddle
     def r_point(self):
         self.r_score += 1
         self.update_scoreboard()
