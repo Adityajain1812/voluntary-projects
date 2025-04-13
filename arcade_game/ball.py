663928e70a4490dcd02c7b2660f2c57d46b08ac7
adityajain1812 <142815869+Adityajain1812@users.noreply.github.com>
2025-04-13 22:32:47 +0530

    Update ball.py

--- arcade_game/ball.py
+++ arcade_game/ball.py
@@ -13,12 +13,13 @@ class Ball(Turtle):
         self.y_move = 10
         self.move_speed = 0.1
 
-    # Creating balll movements along diagonal x and y axis
+    # Function for ball to movements along diagonal x and y axis
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
+    #  Function to bounce the ball as it touches the paddle and slightly increase its speed
     def bounce_y(self):
         self.y_move *= -1
 
@@ -26,6 +27,7 @@ class Ball(Turtle):
         self.x_move *= -1
         self.move_speed *= 0.9
 
+    # Reseting ball position to (0, 0) as any paddle misses the ball and reseting balls speed to normal 
     def reset_position(self):
         self.goto(0, 0)
         self.move_speed = 0.1
