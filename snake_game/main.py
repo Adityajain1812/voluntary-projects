00c634e16c270129bc83d306d00103475871ad20
adityajain1812 <142815869+Adityajain1812@users.noreply.github.com>
2025-04-16 22:43:29 +0530

    Update main.py

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,52 +1,55 @@
-# Importing libraries 
 from turtle import Screen
 import time
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 
-# Creating Background 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+def main():
+    # Creating background
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+    screen.tracer(0)
 
-# Assigning keys for the movement of snake
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
+    # Assigning keys for the movement of snake
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
-    # Detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
+        # Detecting collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
 
-    # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.reset()
-        snake.reset()
-
-    # Detect self collision
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+        # Detecting collision with wall
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
             scoreboard.reset()
             snake.reset()
 
+        # Detecting self collision
+        for segment in snake.segments:
+            if segment == snake.head:
+                pass
+            elif snake.head.distance(segment) < 10:
+                scoreboard.reset()
+                snake.reset()
 
-screen.exitonclick()
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    main()
